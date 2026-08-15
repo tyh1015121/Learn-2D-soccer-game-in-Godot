b428154696d2afe6482b92cb3067dbba7cfe3672
LittleYou <280033323@qq.com>
2026-08-15 17:04:43 +0800

    added node-based state machine

--- utils/key_utils.gd
+++ utils/key_utils.gd
@@ -25,11 +25,11 @@ func get_input_vector(scheme: Player.ControlScheme) -> Vector2:
 	var map : Dictionary = ACTIONS_MAP[scheme]
 	return Input.get_vector(map[Action.LEFT],map[Action.RIGHT],map[Action.UP],map[Action.DOWN])
 	
-func is_asction_pressed(scheme: Player.ControlScheme, action: Action) -> bool:
+func is_action_pressed(scheme: Player.ControlScheme, action: Action) -> bool:
 	return Input.is_action_pressed(ACTIONS_MAP[scheme][action])
 	
-func is_asction_just_pressed(scheme: Player.ControlScheme, action: Action) -> bool:
+func is_action_just_pressed(scheme: Player.ControlScheme, action: Action) -> bool:
 	return Input.is_action_just_pressed(ACTIONS_MAP[scheme][action])
 	
-func is_asction_just_released(scheme: Player.ControlScheme, action: Action) -> bool:
+func is_action_just_released(scheme: Player.ControlScheme, action: Action) -> bool:
 	return Input.is_action_just_released(ACTIONS_MAP[scheme][action])
