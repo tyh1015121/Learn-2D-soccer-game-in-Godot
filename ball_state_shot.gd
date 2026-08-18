71e7a752fae0c70214f4cb286b83a9974746d2ea
Qing <280033323@qq.com>
2026-08-18 15:36:24 +0800

    added ability to shoot the ball

--- ball_state_shot.gd
+++
@@ -1,2 +0,0 @@
-class_name BallStateShot
-extends BallState
--- scenes/characters/character_states/player_state_moving.gd
+++ scenes/characters/character_states/player_state_moving.gd
@@ -11,11 +11,11 @@ func _process(_delta: float) -> void:
 	player.set_heading()
 
 func handle_human_movement() -> void:
-	var direction := KeyUtils.get_input_vector(player.control_scheme)
+	var direction :Vector2= KeyUtils.get_input_vector(player.control_scheme)
 	player.velocity = direction * player.speed
 	
 	if player.has_ball() and KeyUtils.is_action_just_pressed(player.control_scheme,KeyUtils.Action.SHOOT):
-		state_transition_requested.emit(Player.State.PREPPING_SHOT)
+		transition_state(Player.State.PREPPING_SHOT)
 
 	#if player.velocity != Vector2.ZERO and KeyUtils.is_action_just_pressed(player.control_scheme,KeyUtils.Action.SHOOT):
 		#state_transition_requested.emit(Player.State.TACKLING)
