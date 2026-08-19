fb0b29381fc40aeee6b3ad583fa82eb002ec028b
LittleYou <280033323@qq.com>
2026-08-19 20:49:59 +0800

    added passing state

--- scenes/characters/character_states/player_state_moving.gd
+++ scenes/characters/character_states/player_state_moving.gd
@@ -13,6 +13,11 @@ func _process(_delta: float) -> void:
 func handle_human_movement() -> void:
 	var direction :Vector2= KeyUtils.get_input_vector(player.control_scheme)
 	player.velocity = direction * player.speed
+	if player.velocity != Vector2.ZERO:
+		teammate_detection_area.rotation = player.velocity.angle()
+		
+	if player.has_ball() and KeyUtils.is_action_just_pressed(player.control_scheme,KeyUtils.Action.PASS):
+		transition_state(Player.State.PASSING)
 	
 	if player.has_ball() and KeyUtils.is_action_just_pressed(player.control_scheme,KeyUtils.Action.SHOOT):
 		transition_state(Player.State.PREPPING_SHOT)
